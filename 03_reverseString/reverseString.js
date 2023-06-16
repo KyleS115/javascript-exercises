8305b8c45869b78583b18e8951966d0599b9602e
KyleS115 <Kyle.Smith115@gmail.com>
2023-06-16 15:27:57 -0400

    Finished javascript exercise #3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(anyTypeInput) {
+    strInput = String(anyTypeInput)
+    strLength = strInput.length
+    strReversed = ""
+    for (let i = strLength; i >= 0; i--) {
+        strReversed += strInput.slice(i - 1, i)
+    }
+    return (strReversed)
 };
 
 // Do not edit below this line
