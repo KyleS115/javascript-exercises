7c4ae319e56dbde3163e4bcd1bd67e31446510ff
KyleS115 <Kyle.Smith115@gmail.com>
2023-06-17 19:44:33 -0400

    Finished javascript exercises #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let lesser = 0
+    let greater = 0
+    if ((typeof num1 === "number") && (typeof num2 === "number") && (num1 >= 0) && (num2 >= 0)) {
+        if (num1 >= num2) {
+            lesser += num2
+            greater += num1
+        } else {
+            lesser += num1
+            greater += num2
+        }
+        let currentSum = 0
+        for (let i = lesser; i <= greater; i++) {
+            currentSum += i
+        };
+        return (currentSum);
+    } else {
+        return ("ERROR");
+    };
 };
 
 // Do not edit below this line
