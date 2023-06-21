19166ad2eeafa7d1776d3d3e81246c0f8d2c1f32
KyleS115 <Kyle.Smith115@gmail.com>
2023-06-21 18:55:25 -0400

    Finished javascript exercise #9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (checkString) {
+    cleanString = checkString.toLowerCase();
+    cleanString = cleanString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+    cleanString = cleanString.replace(/\s/g, '');
+    reverseString = '';
+    for (let b = checkString.length; b > 0; b--) {
+        reverseString += cleanString.slice(b - 1, b);
+    };
+    if (cleanString === reverseString) {
+        return (true);
+    } else {
+        return (false);
+    };
 };
 
 // Do not edit below this line
