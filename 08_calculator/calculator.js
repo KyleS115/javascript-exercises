c1a7f0d047e6e913e87aa3cbea04e3b5c662f5ff
KyleS115 <Kyle.Smith115@gmail.com>
2023-06-21 16:53:06 -0400

    Finished javascript exercise #8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return (num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfNumbers) {
+	const sumOfNumbers = arrayOfNumbers.reduce((currentSum, currentNum) => {
+    return (currentSum += currentNum);
+  }, 0);
+  return (sumOfNumbers);
 };
 
 const multiply = function() {
-
+  if (arguments.length === 0) {
+    return (0);
+  } else {
+    let runningProduct = 1;
+    for (let i = 0; i < arguments.length; i++) {
+      runningProduct *= arguments[i];
+    };
+    return (runningProduct);
+  };
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let runningProduct = 1;
+  for (let i = 1; i <= num2; i++) {
+    runningProduct *= num1;
+  };
+  return (runningProduct);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let runningProduct = 1;
+  for (i = num; i > 0; i--) {
+    runningProduct *= i;
+  }
+  return (runningProduct);
 };
 
 // Do not edit below this line
