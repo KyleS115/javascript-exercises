951b31c037744b753a0d21342773e88b152b3c47
KyleS115 <Kyle.Smith115@gmail.com>
2023-06-21 20:50:05 -0400

    Finished javascript exercise #10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(arrayLength) {
+    if (parseInt(arrayLength) > 0) {
+        fiboArray = [1, 1];
+        fiboArray.length = parseInt(arrayLength);
+        for (let i = 2; i < parseInt(arrayLength); i++) {
+            fiboArray[i] = fiboArray[i - 1] + fiboArray[i - 2];
+        };
+        return fiboArray[parseInt(arrayLength) - 1];
+    } else {
+        return 'OOPS';
+    };
+    
 };
 
 // Do not edit below this line
