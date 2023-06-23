7cc3e017c59c86c12f6274a62ff49a77cbdbbb62
KyleS115 <Kyle.Smith115@gmail.com>
2023-06-23 13:03:42 -0400

    Finsished javascript exercise #12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,12 +1,14 @@
 function findTheOldest(people) {
-    oldest = {
-        yearOfBirth: 0,
-        yearOfDeath: 0,
-    };
-    people.reduce((0, people) => {
-        if
-    })
-    
+    let peopleEdited = people
+    return peopleEdited.reduce((currentOldest, person) => {
+        if (("yearOfDeath" in person === false) && (new Date().getFullYear() - person.yearOfBirth > currentOldest.yearOfDeath - currentOldest.yearOfBirth)) {
+            person.yearOfDeath = new Date().getFullYear()
+            return person;
+        } else if (person.yearOfDeath - person.yearOfBirth > currentOldest.yearOfDeath - currentOldest.yearOfBirth) {
+            return person;
+            };
+        return currentOldest;
+    }, {yearOfBirth: 0, yearOfDeath: 0});
 };
 
 // Do not edit below this line
